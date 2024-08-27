9b7bf5a0a568abb5fc4685fd0206556589b21a07
Gabriel <xlbiellx@icloud.com>
2024-08-26 21:13:39 -0300

    first commit

---
+++ frontend/src/components/ConfirmationModal/index.js
@@ -0,0 +1,55 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import Typography from "@material-ui/core/Typography";
+
+import { i18n } from "../../translate/i18n";
+
+const ConfirmationModal = ({ title, children, open, onClose, onConfirm }) => {
+	return (
+		<Dialog
+			open={open}
+			onClose={() => onClose(false)}
+			aria-labelledby="confirm-dialog"
+		>
+			<DialogTitle id="confirm-dialog">{title}</DialogTitle>
+			<DialogContent dividers>
+				<Typography>{children}</Typography>
+			</DialogContent>
+			<DialogActions>
+				<Button
+					variant="contained"
+					onClick={() => onClose(false)}
+ 					style={{
+                                        color: "white",
+                                        backgroundColor: "#b33966",
+                                        boxShadow: "none",
+                                        borderRadius: 0
+                                         }}
+				>
+					{i18n.t("confirmationModal.buttons.cancel")}
+				</Button>
+				<Button
+					variant="contained"
+					onClick={() => {
+						onClose(false);
+						onConfirm();
+					}}
+					style={{
+                                        color: "white",
+                                        backgroundColor: "#1e7eba",
+                                        boxShadow: "none",
+                                        borderRadius: 0
+                                         }}
+				>
+					{i18n.t("confirmationModal.buttons.confirm")}
+				</Button>
+			</DialogActions>
+		</Dialog>
+	);
+};
+
+export default ConfirmationModal;
